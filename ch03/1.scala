9f4ebe0fb35dcca7291b950351f39276aa319314
agiledon <bruce.e.zhang@gmail.com>
2013-09-03 21:23:19 +0800

    use yield

--- ch03/1.scala
+++ ch03/1.scala
@@ -1,11 +1,12 @@
+import scala.util.Random
+
 def generateArrayWithRandomNumber(n: Int) = {
-	val resultArray = new Array[Int](n)
-	for (index <- 0 until n) resultArray(index) = scala.util.Random.nextInt(n)
-	resultArray
+	//val resultArray = new Array[Int](n)
+	for (index <- 0 until n) yield Random.nextInt(n)
+	//resultArray
 }
 
 val array = generateArrayWithRandomNumber(10)
-println(array)
 println(array.mkString(", "))
 val sorted = array.sorted
 println(sorted.mkString(", "))
