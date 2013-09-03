e2185696225dd6cd87d741d8d9aa614c585f177c
agiledon <bruce.e.zhang@gmail.com>
2013-09-03 21:58:19 +0800

    modify exercise 10 for chapter 02

--- ch02/10.scala
+++ ch02/10.scala
@@ -1,12 +1,17 @@
-def xn(n : Int): Int = {
+def func(x: Int, n : Int): Int = {
 	if (n == 0) 1
-	else if (n == 1) 2
-	else if (n > 0 && n % 2 == 0) xn(n / 2) * xn(n / 2)
-	else if (n > 0 && n % 2 == 1) xn(1) * xn(n - 1)
-	else 1 / xn(-1 * n)
+	else {
+		if (n > 0) {
+			if (n % 2 == 0) {
+				 func(x, n/2) * func(x, n/2)
+			} else {
+				x * func(x, n - 1)
+			}
+		}else (1/func(x, -n))
+	}
 }
 
-println(xn(2))
-println(xn(20))
-println(xn(3))
-println(xn(-5))
+println(func(10, 2))
+println(func(10, 3))
+println(func(10, 0))
+println(func(10, -2))
